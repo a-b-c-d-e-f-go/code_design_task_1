b07ef1a3db3ae49cf9a6482455a6f8b995a053eb
a-b-c-d-e-f-go <s233275@students.aie.edu.au>
2024-06-14 12:16:55 +1000

    Update AL.h
    More descriptive const comments.

--- CDDS_Optimise/AL.h
+++ CDDS_Optimise/AL.h
@@ -23,14 +23,20 @@ ifv (arr) {									\
 
 using namespace std;
 
-//Static constant sizes assigned before running the program.
+
+// All of the static variables that determine the size/attributes/performance of the program before runtime.
+
+//How much the map is partitioned/subdivided. Keep in mind these are both used to determine the size of a 2D array, so even small changes affect memory usage significantly.
 static const size_t MAP_COLUMNS = 12;
 static const size_t MAP_ROWS = 12;
 
+//Width and height of the screen - used by the map to size the nodes and by raylib to initialize the window.
 static const size_t SCREEN_WIDTH = 800;
 static const size_t SCREEN_HEIGHT = 450;
 
+//Width and height of individual nodes in the map - calculated based on screen size / how much it is subdivided.
 static const size_t NODE_WIDTH = SCREEN_WIDTH / MAP_COLUMNS;
 static const size_t NODE_HEIGHT = SCREEN_HEIGHT / MAP_ROWS;
+
 static const size_t CRITTER_COUNT = 51; //Includes the destroyer (51 is 50 critters and 1 destroyer).
-static const size_t MAX_VELOCITY = 80;
+static const size_t MAX_VELOCITY = 80; //The speed critters move at.
